fd0af86e4279b4c715ee06850a33be619d28331b
monnoroch <monnoroch@gmail.com>
2015-07-16 22:48:10 +0300

    integrated graceful restart server  into downloader

--- crawler/downloader/main.go
+++ crawler/downloader/main.go
@@ -4,15 +4,20 @@ import (
 	"flag"
 	"io/ioutil"
 	"net/http"
+	"os"
+	"os/signal"
 	"psearch/util"
 	"psearch/util/errors"
+	"psearch/util/graceful"
 	"psearch/util/log"
 	"strconv"
+	"syscall"
 )
 
 func main() {
 	var help = flag.Bool("help", false, "print help")
 	var port = flag.Int("port", -1, "port to listen")
+	var gracefulRestart = graceful.SetFlag()
 	flag.Parse()
 
 	if *help || *port == -1 {
@@ -20,7 +25,13 @@ func main() {
 		return
 	}
 
-	http.HandleFunc("/dl", util.CreateErrorHandler(func(w http.ResponseWriter, r *http.Request) error {
+	server := graceful.NewServer(http.Server{})
+
+	http.HandleFunc("/favicon.ico", graceful.CreateHandler(server, util.CreateErrorHandler(func(w http.ResponseWriter, r *http.Request) error {
+		return nil
+	})))
+
+	http.HandleFunc("/dl", graceful.CreateHandler(server, util.CreateErrorHandler(func(w http.ResponseWriter, r *http.Request) error {
 		r.ParseForm()
 		url, err := util.GetParam(r, "url")
 		if err != nil {
@@ -43,9 +54,31 @@ func main() {
 			log.Println("Served URL: " + url)
 		}
 		return err
-	}))
+	})))
 
-	if err := http.ListenAndServe(":"+strconv.Itoa(*port), nil); err != nil {
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, syscall.SIGHUP)
+	go func() {
+		for {
+			s := <-signals
+			switch s {
+			case syscall.SIGHUP:
+				server.Stop()
+				log.Println("stopped!")
+			default:
+				log.Println("Unknown signal.")
+			}
+		}
+	}()
+
+	log.Println("serving!")
+	if err := server.ListenAndServe(":"+strconv.Itoa(*port), *gracefulRestart); err != nil {
+		log.Println("served err!")
 		log.Fatal(errors.NewErr(err))
+	}
+
+	log.Println("served ok!")
+	if err := server.Restart(); err != nil {
+		log.Fatal(err)
 	}
 }
