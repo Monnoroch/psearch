5b08283aef29756286ecb15c846e4b4a2f7c6e65
monnoroch <monnoroch@gmail.com>
2015-07-16 18:05:40 +0300

    errors package

---
+++ util/errors/errors.go
@@ -0,0 +1,29 @@
+package errors
+
+import (
+	goerrors "errors"
+	"runtime/debug"
+)
+
+type ErrorT struct {
+	Base      error
+	callstack string
+}
+
+func (self ErrorT) Error() string {
+	return self.Base.Error() + "\n" + self.callstack
+}
+
+func New(s string) error {
+	return ErrorT{goerrors.New(s), string(debug.Stack())}
+}
+
+func NewErr(err error) error {
+	if err == nil {
+		return nil
+	}
+	if _, ok := err.(ErrorT); ok {
+		return err
+	}
+	return ErrorT{err, string(debug.Stack())}
+}
